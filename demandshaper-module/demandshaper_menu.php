56b919476fd836dc997f24ab5efef76d6277bea6
TrystanLea <glyn.hudson@openenergymonitor.org>
2018-12-03 12:19:17 +0000

    add menu

---
+++ demandshaper-module/demandshaper_menu.php
@@ -0,0 +1,25 @@
+<?php
+
+    $domain = "messages";
+
+    $menu_left[] = array(
+        'id'=>"demandshaper_menu",
+        'name'=>"DemandShaper", 
+        'path'=>"demandshaper" , 
+        'session'=>"write", 
+        'order' => 0,
+        'icon'=>'icon-home icon-white',
+        'hideinactive'=>1
+    );
+
+    $menu_dropdown[] = array(
+        'id'=>"demandshaper_menu_extras",
+        'name'=>"DemandShaper", 
+        'path'=>"demandshaper" , 
+        'session'=>"write", 
+        'order' => 0,
+        'icon'=>'icon-home'
+    );
+    
+    
+
