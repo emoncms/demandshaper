b4ba12f0f5d486207197f1b4ed271350dcdaa1c4
TrystanLea <trystan.lea@gmail.com>
2019-02-25 12:45:22 +0000

    revert menu

--- demandshaper-module/demandshaper_menu.php
+++ demandshaper-module/demandshaper_menu.php
@@ -1,19 +1,4 @@
 <?php
-    global $demandshaper_users,$session;
-    $domain = "messages";
-    if (($session["write"]) && in_array($session["userid"],$demandshaper_users)) {
-    $menu_left[] = array(
-        'id'=>"demandshaper_menu",
-        'name'=>"Devices", 
-        'path'=>"demandshaper" , 
-        'session'=>"write", 
-        'order' => 4,
-        'icon'=>'icon-calendar icon-white',
-        'hideinactive'=>0
-    );
-    }
-
-    /*
     $menu_dropdown[] = array(
         'id'=>"demandshaper_menu_extras",
         'name'=>"My Devices", 
@@ -22,7 +7,3 @@
         'order' => 0,
         'icon'=>'icon-home'
     );
-    */
-    
-    
-
