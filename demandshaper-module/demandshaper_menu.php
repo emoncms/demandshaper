4fce292856b1d654b762e216c6868a19079c336a
TrystanLea <glyn.hudson@openenergymonitor.org>
2019-02-25 12:53:18 +0000

    fix menu

--- demandshaper-module/demandshaper_menu.php
+++ demandshaper-module/demandshaper_menu.php
@@ -1,8 +1,17 @@
 <?php
-
+    $domain = "messages";
+    $menu_left[] = array(
+        'id'=>"demandshaper_menu",
+        'name'=>"DemandShaper", 
+        'path'=>"demandshaper" , 
+        'session'=>"write", 
+        'order' => 0,
+        'icon'=>'icon-home icon-white',
+        'hideinactive'=>1
+    );
     $menu_dropdown[] = array(
         'id'=>"demandshaper_menu_extras",
-        'name'=>"My Devices", 
+        'name'=>"DemandShaper", 
         'path'=>"demandshaper" , 
         'session'=>"write", 
         'order' => 0,
