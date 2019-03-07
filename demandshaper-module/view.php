e86214141eb382783f964aca145c22ffb8c118ac
TrystanLea <trystan.lea@gmail.com>
2019-03-07 11:13:27 +0000

    improved user feedback for setup steps

--- demandshaper-module/view.php
+++ demandshaper-module/view.php
@@ -56,29 +56,6 @@ if (window.session!=undefined) {
     </div>
   </div>
   
-  <script>
-  init_sidebar({menu_element:"#demandshaper_menu"});
-  $.ajax({ url: emoncmspath+"device/list.json", dataType: 'json', async: false, success: function(result) { 
-      // Associative array of devices by nodeid
-      devices = {};
-      var out = "";
-      for (var z in result) {
-          if (result[z].type=="openevse" || result[z].type=="smartplug" || result[z].type=="hpmon") {
-              devices[result[z].nodeid] = result[z];
-              // sidebar list
-              out += "<li><a href='"+path+"demandshaper?node="+result[z].nodeid+"'><span class='icon-"+result[z].type+"'></span>"+ucfirst(result[z].nodeid)+"</a></li>";
-              // select first device if device is not defined
-              if (device=="") device = result[z].nodeid;
-          }
-      }
-      
-      $(".sidenav-menu").html(out);
-  }});
-
-  function ucfirst(string) {
-      return string.charAt(0).toUpperCase() + string.slice(1);
-  }
-  </script>
   <!------------------------------------------------------------------------------------------------------>
   
   <div id="scheduler-top"></div>
@@ -87,10 +64,59 @@ if (window.session!=undefined) {
       <i class="icon-exclamation-sign icon-white"></i> Device on ip address: <span id="auth-check-ip"></span> would like to connect 
       <button class="btn btn-small auth-check-btn auth-check-allow">Allow</button>
   </div>
-  
+
+  <div id="no-devices-found" class="hide">
+
+      <h2 id="no-devices-found-title">No Devices Found</h2>
+            
+      <div style="display:inline-block; padding:10px"><span class='icon-smartplug'></span></div>
+      <div style="display:inline-block; padding:10px"><span class='icon-hpmon'></span></div>
+      <div style="display:inline-block; padding:10px"><span class='icon-openevse'></span></div>
+      <div style="height:10px"></div>
+
+      <div id="no-devices-found-checking">
+          <p>Checking for pairing request</p><br>
+          <img src="<?php echo $path; ?>Modules/demandshaper/ajax-loader.gif">
+      </div>
+  </div>
+
   <?php
       include "Modules/demandshaper/general.php";
   ?>
+
+  <script>
+  init_sidebar({menu_element:"#demandshaper_menu"});
+  
+  device_loaded = false;
+  
+  update_sidebar();
+  setInterval(update_sidebar,10000);
+  function update_sidebar() {
+      $.ajax({ url: emoncmspath+"device/list.json", dataType: 'json', async: true, success: function(result) { 
+          // Associative array of devices by nodeid
+          devices = {};
+          var out = "";
+          for (var z in result) {
+              if (result[z].type=="openevse" || result[z].type=="smartplug" || result[z].type=="hpmon") {
+                  devices[result[z].nodeid] = result[z];
+                  // sidebar list
+                  out += "<li><a href='"+path+"demandshaper?node="+result[z].nodeid+"'><span class='icon-"+result[z].type+"'></span>"+ucfirst(result[z].nodeid)+"</a></li>";
+                  // select first device if device is not defined
+                  if (device=="") device = result[z].nodeid;
+              }
+          }
+          
+          $(".sidenav-menu").html(out);
+          if (!device_loaded) load_device();
+      }});
+  }
+
+  function ucfirst(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  </script>
+  
+
 </div>
 <script>
 var emoncmspath = "<?php echo $emoncmspath; ?>";
@@ -98,13 +124,15 @@ var emoncmspath = "<?php echo $emoncmspath; ?>";
 // Device authentication transfer
 // -------------------------------------------------------------------------------------------------------
 auth_check();
-setInterval(auth_check,10000);
+setInterval(auth_check,5000);
 function auth_check(){
     $.ajax({ url: emoncmspath+"device/authcheck.json", dataType: 'json', async: true, success: function(data) {
         if (typeof data.ip !== "undefined") {
             $("#auth-check-ip").html(data.ip);
             $("#auth-check").show();
             $("#table").css("margin-top","0");
+            $("#no-devices-found-title").html("Device Found");
+            $("#no-devices-found-checking").html("Click Allow to pair device");
         } else {
             $("#table").css("margin-top","3rem");
             $("#auth-check").hide();
@@ -116,6 +144,7 @@ $(".auth-check-allow").click(function(){
     var ip = $("#auth-check-ip").html();
     $.ajax({ url: emoncmspath+"device/authallow.json?ip="+ip, dataType: 'json', async: true, success: function(data) {
         $("#auth-check").hide();
+        $("#no-devices-found-checking").html("Please wait for device to connect");
     }});
 });
 </script>
