457ca33badd4bb13a2c72e6cc4d1202f4d2d1563
Emrys <emrys@openenergymonitor.org>
2019-04-05 17:40:11 +0100

    added dark-sidebar menu

--- demandshaper-module/demandshaper_menu.php
+++ demandshaper-module/demandshaper_menu.php
@@ -1,19 +1,6 @@
 <?php
-    $domain = "messages";
-    $menu_left[] = array(
-        'id'=>"demandshaper_menu",
-        'name'=>"DemandShaper", 
-        'path'=>"demandshaper" , 
-        'session'=>"write", 
-        'order' => 4,
-        'icon'=>'icon-calendar icon-white',
-        'hideinactive'=>0
-    );
-    $menu_dropdown[] = array(
-        'id'=>"demandshaper_menu_extras",
-        'name'=>"DemandShaper", 
-        'path'=>"demandshaper" , 
-        'session'=>"write", 
-        'order' => 0,
-        'icon'=>'icon-calendar'
+    $menu['sidebar']['setup'][] = array(
+        'text' => _("DemandShaper"),
+        'path' => 'demandshaper',
+        'icon' => 'calendar'
     );
