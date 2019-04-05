011e88f5dbe911598943159ef0dffe70f4951628
Trystan Lea <trystan.lea@gmail.com>
2019-04-05 15:40:44 +0100

    improved service script installation

--- install.sh
+++ install.sh
@@ -2,22 +2,26 @@
 echo "DemandShaper module installation script"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# ---------------------------------------------------------
+# Install service
+# ---------------------------------------------------------
 service=demandshaper
 
-if [ ! -f /lib/systemd/system/$service.service ]; then
-    echo "- installing $service.service"
-    sudo cp $DIR/$service.service /lib/systemd/system
-    sudo sed -i "s~ExecStart=.*~ExecStart=/usr/bin/php $DIR/run.php~" /lib/systemd/system/$service.service
-    sudo systemctl enable $service.service
-    sudo systemctl start $service.service
-else
+if [ -f /lib/systemd/system/$service.service ]; then
     echo "- reinstalling $service.service"
+    sudo systemctl stop $service.service
+    sudo systemctl disable $service.service
     sudo rm /lib/systemd/system/$service.service
-    sudo cp $DIR/$service.service /lib/systemd/system
-    sudo sed -i "s~ExecStart=.*~ExecStart=/usr/bin/php $DIR/run.php~" /lib/systemd/system/$service.service
-    sudo systemctl daemon-reload
-    sudo systemctl restart $service.service
+else
+    echo "- installing $service.service"
 fi
+
+sudo cp $DIR/$service.service /lib/systemd/system
+sudo sed -i "s~ExecStart=.*~ExecStart=/usr/bin/php $DIR/run.php~" /lib/systemd/system/$service.service
+sudo systemctl enable $service.service
+sudo systemctl restart $service.service
 
 state=$(systemctl show $service | grep ActiveState)
 echo "- Service $state"
+
+# ---------------------------------------------------------
