0aaefe932793cdd46863ccf81f9b35b9a2fe6277
TrystanLea <trystan.lea@gmail.com>
2020-02-28 09:16:11 +0000

    check for undefined ctrl mode

--- demandshaper-module/general.js
+++ demandshaper-module/general.js
@@ -526,7 +526,11 @@ function load_device(device_id, device_name, device_type)
                 } else {
                     state_matched = true;
                     
-                    device_ctrl_mode = result.ctrl_mode.toLowerCase();
+                    if (result.ctrl_mode!=undefined) {
+                        device_ctrl_mode = result.ctrl_mode.toLowerCase();
+                    } else {
+                        device_ctrl_mode = "timer";
+                    }
                     if (schedule.settings.ctrlmode!=device_ctrl_mode) state_matched = false;
                     if (schedule.settings.ctrlmode=="smart" && device_ctrl_mode=="timer") state_matched = true;
                     if (!state_matched) console.log(schedule.settings.ctrlmode+"!="+device_ctrl_mode)
