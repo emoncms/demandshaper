ca550deaa87f928deb5668059be0cd6c8118ff48
TrystanLea <trystan.lea@gmail.com>
2020-05-21 17:39:43 +0100

    load all octopus agile regions

--- emoncms-remote/demandshaper_controller.php
+++ emoncms-remote/demandshaper_controller.php
@@ -24,7 +24,16 @@ function demandshaper_controller()
     if ($route->action == 'carbonintensity') $result = json_decode($redis->get("demandshaper:carbonintensity"));
     
     // "https://api.octopus.energy/v1/products/AGILE-18-02-21/electricity-tariffs/E-1R-AGILE-18-02-21-D/standard-unit-rates/"
-    else if ($route->action == 'octopus') $result = json_decode($redis->get("demandshaper:octopus"));
+    else if ($route->action == 'octopus') {
+        $gsp_id = "D";
+        if (isset($_GET['gsp'])) {
+            if (in_array($_GET['gsp'],array("A","B","C","D","E","F","G","H","J","K","L","M","N","P"))) {
+                $gsp_id = $_GET['gsp'];
+            }
+        }
+        
+        $result = json_decode($redis->get("demandshaper:octopus:$gsp_id"));
+    }
 
     return array('content'=>$result);
 }
