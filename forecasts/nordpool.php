2c22364bdbb31e4b06953baacd25fe77f3a766f4
TrystanLea <trystan.lea@gmail.com>
2020-05-22 14:00:40 +0100

    refactor nordpool forecast

--- forecasts/nordpool.php
+++ forecasts/nordpool.php
@@ -4,48 +4,78 @@ function get_list_entry_nordpool()
 {
     return array(
         "category"=>"Nordpool Spot",
-        "name"=>"DK1",
+        "name"=>"Nordpool Spot",
         "currency"=>"DKK",
-        "vat"=>"25"
+        "vat"=>"25",
+        "params"=>array(
+            "area"=>array(
+                "type"=>"select",
+                "options"=>array(
+                    "DK1"=>"DK1",
+                    "DK2"=>"DK2",
+                    "EE"=>"EE",
+                    "FI"=>"FI",
+                    "LT"=>"LT",
+                    "NO1"=>"NO1",
+                    "NO2"=>"NO2",
+                    "NO3"=>"NO3",
+                    "NO4"=>"NO4",
+                    "NO5"=>"NO5",
+                    "SE1"=>"SE1",
+                    "SE2"=>"SE2",
+                    "SE3"=>"SE3",
+                    "SE4"=>"SE4"
+                )
+            )
+        )
     );
 }
 
-/*
-$forecast_list["nordpool_dk1"] = array("category"=>"Nordpool Spot","name"=>"DK1","currency"=>"DKK","vat"=>"25");
-$forecast_list["nordpool_dk2"] = array("category"=>"Nordpool Spot","name"=>"DK2","currency"=>"DKK","vat"=>"25");
-$forecast_list["nordpool_ee"] = array("category"=>"Nordpool Spot","name"=>"EE","currency"=>"EUR","vat"=>"20");
-$forecast_list["nordpool_fi"] = array("category"=>"Nordpool Spot","name"=>"FI","currency"=>"EUR","vat"=>"24");
-$forecast_list["nordpool_lt"] = array("category"=>"Nordpool Spot","name"=>"LT","currency"=>"EUR","vat"=>"21");
-$forecast_list["nordpool_no1"] = array("category"=>"Nordpool Spot","name"=>"NO1","currency"=>"NOK","vat"=>"25");
-$forecast_list["nordpool_no2"] = array("category"=>"Nordpool Spot","name"=>"NO2","currency"=>"NOK","vat"=>"25");
-$forecast_list["nordpool_no3"] = array("category"=>"Nordpool Spot","name"=>"NO3","currency"=>"NOK","vat"=>"25");
-$forecast_list["nordpool_no4"] = array("category"=>"Nordpool Spot","name"=>"NO4","currency"=>"NOK","vat"=>"25");
-$forecast_list["nordpool_no5"] = array("category"=>"Nordpool Spot","name"=>"NO5","currency"=>"NOK","vat"=>"25");
-$forecast_list["nordpool_se1"] = array("category"=>"Nordpool Spot","name"=>"SE1","currency"=>"SEK","vat"=>"25");
-$forecast_list["nordpool_se2"] = array("category"=>"Nordpool Spot","name"=>"SE2","currency"=>"SEK","vat"=>"25");
-$forecast_list["nordpool_se3"] = array("category"=>"Nordpool Spot","name"=>"SE3","currency"=>"SEK","vat"=>"25");
-$forecast_list["nordpool_se4"] = array("category"=>"Nordpool Spot","name"=>"SE4","currency"=>"SEK","vat"=>"25");
-*/
-
 function get_forecast_nordpool($redis,$params)
 {
+    if (!isset($params->area)) return false;
+    if (!isset($params->signal_token)) return false;
+    
+    $list_entry = get_list_entry_nordpool();
+    if (!isset($list_entry["params"]["area"]["options"][$params->area])) return false;
+    
+    $nordpool = array(;
+        "DK1"=>array("currency"=>"DKK","vat"=>"25");
+        "DK2"=>array("currency"=>"DKK","vat"=>"25");
+        "EE"=>array("currency"=>"EUR","vat"=>"20");
+        "FI"=>array("currency"=>"EUR","vat"=>"24");
+        "LT"=>array("currency"=>"EUR","vat"=>"21");
+        "NO1"=>array("currency"=>"NOK","vat"=>"25");
+        "NO2"=>array("currency"=>"NOK","vat"=>"25");
+        "NO3"=>array("currency"=>"NOK","vat"=>"25");
+        "NO4"=>array("currency"=>"NOK","vat"=>"25");
+        "NO5"=>array("currency"=>"NOK","vat"=>"25");
+        "SE1"=>array("currency"=>"SEK","vat"=>"25");
+        "SE2"=>array("currency"=>"SEK","vat"=>"25");
+        "SE3"=>array("currency"=>"SEK","vat"=>"25");
+        "SE4"=>array("currency"=>"SEK","vat"=>"25");
+    );
+
     $list_entry = get_list_entry_nordpool();
     
     $result = json_decode($redis->get("demandshaper:$signal"));
 
     if (!$result || !is_object($result)) {
-        $area = $list_entry["name"];
-        $currency = $list_entry["currency"];
-        $time = time();
         
-        if ($result = http_request("GET","http://datafeed.expektra.se/datafeed.svc/spotprice?token=$signal_token&bidding_area=$area&format=json&perspective=$currency&$time",array())) {
+        $params_req = array(
+            "token"=>$params->signal_token,
+            "bidding_area"=>$params->area,
+            "perspective"=>$nordpool[$params->area]["currency"],
+            "format"=>"json"
+            "t"=>time()
+        );
+        
+        if ($result = http_request("GET","http://datafeed.expektra.se/datafeed.svc/spotprice",$params_req)) {
             $r = json_decode($result);
-
             if(null!=$r) {
                 $redis->set("demandshaper:$signal",$result);
                 $redis->expire("demandshaper:$signal",1800);
             }
-
             $result = $r;
         }
     }
@@ -54,7 +84,7 @@ function get_forecast_nordpool($redis,$params)
      
     if ($result!=null && isset($result->data)) {
 
-        $vat = $list_entry["vat"];
+        $vat = $nordpool[$params->area]["vat"];
         $timestamp = $params->start;
         
         foreach ($result->data as $row) {
