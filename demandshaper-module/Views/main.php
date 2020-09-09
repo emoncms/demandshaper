5e044c3f05ba6c9c785c4b0f98437fea1b4b4675
TrystanLea <trystan.lea@gmail.com>
2020-09-09 19:12:16 +0100

    force cache reload

--- demandshaper-module/Views/main.php
+++ demandshaper-module/Views/main.php
@@ -1,4 +1,4 @@
-<?php global $path; $v=4; ?>
+<?php global $path; $v=5; ?>
 <link rel="stylesheet" href="<?php echo $path; ?>Modules/demandshaper/demandshaper.css?v=<?php echo $v; ?>">
 
 <script language="javascript" type="text/javascript" src="<?php echo $path; ?>Lib/flot/jquery.flot.min.js"></script>
